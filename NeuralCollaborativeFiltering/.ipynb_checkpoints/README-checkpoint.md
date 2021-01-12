8e6fadab8f75a1cbc484b8d58710f23c222fbd54
Namkyeong <namkyeong96@naver.com>
2021-01-12 11:34:50 +0900

    implementing NeuralCF

--- NeuralCollaborativeFiltering/.ipynb_checkpoints/README-checkpoint.md
+++ NeuralCollaborativeFiltering/.ipynb_checkpoints/README-checkpoint.md
@@ -3,7 +3,14 @@ implementation of Recommendation System papers
 
 ## Neural Collaborative Filtering
 Implementation of 'Neural Collaborative Filtering'   
+> He, Xiangnan, et al. "Neural collaborative filtering." Proceedings of the 26th International Conference on World Wide Web. International World Wide Web Conferences Steering Committee, 2017.
+
 
 * [paper](https://arxiv.org/abs/1708.05031)
 * [Author : Dr.Xiangnan He](http://staff.ustc.edu.cn/~hexn/)
 * [Author_Code](https://github.com/hexiangnan/neural_collaborative_filtering)
+
+
+## References
+* [yihong-chen github](https://github.com/yihong-chen/neural-collaborative-filtering)
+* [guoyang9 github](https://github.com/guoyang9/NCF)
---
+++ NeuralCollaborativeFiltering/evaluate.py
@@ -0,0 +1,22 @@
+import numpy as np
+import torch
+
+
+def cal_metrics(model, test_data):
+    hit = 0
+    ndcg = 0
+    for i in range(6040):
+        test_pred = torch.FloatTensor(model(test_data[0][i].cuda(), test_data[1][i].cuda()).cpu()).view(1, -1)
+        neg_pred = torch.FloatTensor(model(test_data[0][i].cuda(), test_data[2][i].cuda()).cpu()).view(1, -1)
+        concat =torch.cat([test_pred, neg_pred], dim = 1)
+        _, indices = torch.topk(concat, 10)
+        indices = indices[0].numpy().tolist()
+        if 0 in indices:
+            hit += 1
+            index = indices.index(0)
+            ndcg += np.reciprocal(np.log2(index+2))
+            
+    hit_ratio = hit / 6040
+    ndcg = ndcg / 6040
+    
+    return hit_ratio, ndcg
