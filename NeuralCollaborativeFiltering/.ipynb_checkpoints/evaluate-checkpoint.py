a65cf70f9bfc92379436dd45918737311ff34a07
Namkyeong <namkyeong96@naver.com>
2021-01-13 19:15:53 +0900

    add NeuralCF

--- NeuralCollaborativeFiltering/.ipynb_checkpoints/evaluate-checkpoint.py
+++ NeuralCollaborativeFiltering/.ipynb_checkpoints/evaluate-checkpoint.py
@@ -3,14 +3,17 @@ import torch
 
 
 def cal_metrics(model, test_data):
+    
     hit = 0
     ndcg = 0
+    
     for i in range(6040):
-        test_pred = torch.FloatTensor(model(test_data[0][i].cuda(), test_data[1][i].cuda()).cpu()).view(1, -1)
-        neg_pred = torch.FloatTensor(model(test_data[0][i].cuda(), test_data[2][i].cuda()).cpu()).view(1, -1)
-        concat =torch.cat([test_pred, neg_pred], dim = 1)
+        test_pred = torch.FloatTensor(model(test_data[0][i].cuda(), test_data[1][i].cuda()).cpu()).view(-1, 1)
+        neg_pred = torch.FloatTensor(model(test_data[0][i].expand(99).cuda(), test_data[2][i].cuda()).cpu())
+        concat = torch.cat([test_pred, neg_pred]).view(-1)
+            
         _, indices = torch.topk(concat, 10)
-        indices = indices[0].numpy().tolist()
+        indices = indices.numpy().tolist()
         if 0 in indices:
             hit += 1
             index = indices.index(0)
